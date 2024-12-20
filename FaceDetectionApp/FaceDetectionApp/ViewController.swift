57dd95552561bdfdb689250792ca1b1a13ca78de
Amy Huang <amyxjhuang@berkeley.edu>
2024-12-19 19:59:40 -0500

    Set up AVCapture starter code and create .gitignore

--- FaceDetectionApp/FaceDetectionApp/ViewController.swift
+++ FaceDetectionApp/FaceDetectionApp/ViewController.swift
@@ -6,18 +6,55 @@
 //
 
 import UIKit
-//import OpenCV
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    var captureSession: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.systemPink
-        
-        // Do any additional setup after loading the view.
+
+        // Setup Capture Session
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .high
+
+        guard let camera = AVCaptureDevice.default(for: .video) else {
+            print("No camera available")
+            return
+        }
+
+        do {
+            let input = try AVCaptureDeviceInput(device: camera)
+            captureSession.addInput(input)
+        } catch {
+            print("Error accessing camera: \(error)")
+            return
+        }
+
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(videoOutput)
+
+        captureSession.startRunning()
     }
 
+    // Process Each Frame
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        // Pass pixelBuffer to OpenCV for processing
+        processImageWithOpenCV(pixelBuffer)
+    }
 
+    func processImageWithOpenCV(_ pixelBuffer: CVPixelBuffer) {
+        // Call OpenCV functions here
+    }
 }
-
