184e0432f1a469fd18bea071552607aefa799cc3
Amy Huang <amyxjhuang@berkeley.edu>
2025-01-03 10:46:58 -0500

    Delete unused UIImage methods

--- FaceDetectionApp/FaceDetectionApp/OpenCV/OpenCVUtils.mm
+++ FaceDetectionApp/FaceDetectionApp/OpenCV/OpenCVUtils.mm
@@ -14,103 +14,8 @@
 static cv::CascadeClassifier faceCascade;
 static bool isModelLoaded = false;
 
-
-@interface UIImage (OpenCVUtils)
-- (void)convertToMat: (cv::Mat *)pMat: (bool)alphaExists;
-- (cv::Mat)cvMatFromUIImage:(UIImage *)image;
-- (cv::Mat)cvMatGrayFromUIImage:(UIImage *)image;
-
-@end
-
-@implementation UIImage (OpenCVUtils)
-
-- (void)convertToMat: (cv::Mat *)pMat: (bool)alphaExists {
-    if (self.imageOrientation == UIImageOrientationRight) {
-        /*
-         * When taking picture in portrait orientation,
-         * convert UIImage to OpenCV Matrix in landscape right-side-up orientation,
-         * and then rotate OpenCV Matrix to portrait orientation
-         */
-        UIImageToMat([UIImage imageWithCGImage:self.CGImage scale:1.0 orientation:UIImageOrientationUp], *pMat, alphaExists);
-        cv::rotate(*pMat, *pMat, cv::ROTATE_90_CLOCKWISE);
-    } else if (self.imageOrientation == UIImageOrientationLeft) {
-        /*
-         * When taking picture in portrait upside-down orientation,
-         * convert UIImage to OpenCV Matrix in landscape right-side-up orientation,
-         * and then rotate OpenCV Matrix to portrait upside-down orientation
-         */
-        UIImageToMat([UIImage imageWithCGImage:self.CGImage scale:1.0 orientation:UIImageOrientationUp], *pMat, alphaExists);
-        cv::rotate(*pMat, *pMat, cv::ROTATE_90_COUNTERCLOCKWISE);
-    } else {
-        /*
-         * When taking picture in landscape orientation,
-         * convert UIImage to OpenCV Matrix directly,
-         * and then ONLY rotate OpenCV Matrix for landscape left-side-up orientation
-         */
-        UIImageToMat(self, *pMat, alphaExists);
-        if (self.imageOrientation == UIImageOrientationDown) {
-            cv::rotate(*pMat, *pMat, cv::ROTATE_180);
-        }
-    }
-}
-
-- (cv::Mat)cvMatFromUIImage:(UIImage *)image
-{
-  CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
-  CGFloat cols = image.size.width;
-  CGFloat rows = image.size.height;
- 
-  cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
- 
-  CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
-                                                 cols,                       // Width of bitmap
-                                                 rows,                       // Height of bitmap
-                                                 8,                          // Bits per component
-                                                 cvMat.step[0],              // Bytes per row
-                                                 colorSpace,                 // Colorspace
-                                                 kCGImageAlphaNoneSkipLast |
-                                                 kCGBitmapByteOrderDefault); // Bitmap info flags
- 
-  CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
-  CGContextRelease(contextRef);
- 
-  return cvMat;
-}
-
-+ (cv::Mat)cvMatGrayFromUIImage:(UIImage *)image
-{
-  CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
-  CGFloat cols = image.size.width;
-  CGFloat rows = image.size.height;
- 
-  cv::Mat cvMat(rows, cols, CV_8UC1); // 8 bits per component, 1 channels
- 
-  CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to data
-                                                 cols,                       // Width of bitmap
-                                                 rows,                       // Height of bitmap
-                                                 8,                          // Bits per component
-                                                 cvMat.step[0],              // Bytes per row
-                                                 colorSpace,                 // Colorspace
-                                                 kCGImageAlphaNoneSkipLast |
-                                                 kCGBitmapByteOrderDefault); // Bitmap info flags
- 
-  CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
-  CGContextRelease(contextRef);
- 
-  return cvMat;
- }
-
-
-
-@end
-
 @implementation OpenCVUtils
 
-
-+ (NSString *)getOpenCVVersion {
-    return [NSString stringWithFormat:@"OpenCV Version %s",  CV_VERSION];
-}
- 
 + (cv::Mat)convertImageBufferToMat:(CVImageBufferRef)imageBuffer {
     if (imageBuffer == NULL) {
         return cv::Mat(); // Return an empty Mat if the buffer is null
@@ -174,16 +79,6 @@ static bool isModelLoaded = false;
     cv::Mat grayMat;
     cv::cvtColor(inputMat, grayMat, cv::COLOR_BGR2GRAY);
 
-//    // Load the Haar cascade model
-//    cv::CascadeClassifier faceCascade;
-//    NSString* faceCascadePath = [[NSBundle mainBundle] pathForResource:@"haarcascade_frontalface_default" ofType:@"xml"];
-//
-//
-//    if (!faceCascade.load([faceCascadePath UTF8String])) {
-//        NSLog(@"Error loading Haar cascade.");
-//        return inputMat; // Return original frame if the model fails to load
-//    }
-
     // Detect faces
     std::vector<cv::Rect> faces;
     float scaleFactor = 1.1;
@@ -239,60 +134,5 @@ static bool isModelLoaded = false;
  
   return finalImage;
  }
-
-
-
-+ (UIImage *)grayscaleImg:(UIImage *)image {
-    cv::Mat mat;
-    [image convertToMat: &mat :false];
-    
-    cv::Mat gray;
-    
-    NSLog(@"channels = %d", mat.channels());
-
-    if (mat.channels() > 1) {
-        cv::cvtColor(mat, gray, CV_RGB2GRAY);
-    } else {
-        mat.copyTo(gray);
-    }
-
-    UIImage *grayImg = MatToUIImage(gray);
-    return grayImg;
-}
-
-+ (UIImage *)resizeImg:(UIImage *)image :(int)width :(int)height :(int)interpolation {
-    cv::Mat mat;
-    [image convertToMat: &mat :false];
-    
-    if (mat.channels() == 4) {
-        [image convertToMat: &mat :true];
-    }
-    
-    NSLog(@"source shape = (%d, %d)", mat.cols, mat.rows);
-    
-    cv::Mat resized;
-    
-//    cv::INTER_NEAREST = 0,
-//    cv::INTER_LINEAR = 1,
-//    cv::INTER_CUBIC = 2,
-//    cv::INTER_AREA = 3,
-//    cv::INTER_LANCZOS4 = 4,
-//    cv::INTER_LINEAR_EXACT = 5,
-//    cv::INTER_NEAREST_EXACT = 6,
-//    cv::INTER_MAX = 7,
-//    cv::WARP_FILL_OUTLIERS = 8,
-//    cv::WARP_INVERSE_MAP = 16
-    
-    cv::Size size = {width, height};
-    
-    cv::resize(mat, resized, size, 0, 0, interpolation);
-    
-    NSLog(@"dst shape = (%d, %d)", resized.cols, resized.rows);
-    
-    UIImage *resizedImg = MatToUIImage(resized);
-    
-    return resizedImg;
-
-}
 
 @end
